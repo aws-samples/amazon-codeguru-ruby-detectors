23ca039f294f30ed774d206107e3aa1d79a5efe5
Rile Ge <gril@amazon.com>
2023-12-15 13:26:07 -0800

    Fix the rule ids of some code examples.

--- src/ruby/detectors/tainted-format/tainted-format-compliant.rb
+++ src/ruby/detectors/tainted-format/tainted-format-compliant.rb
@@ -3,7 +3,7 @@
  SPDX-License-Identifier: Apache-2.0
 =end
 
-# {fact rule=untrusted-format-strings@v1.0 defects=0}
+# {fact rule=tainted-format@v1.00 defects=0}
 class TaintedFormat < ActionController::Base
   def sanitized_input
     # Compliant: User-input is not used to format output.
--- src/ruby/detectors/tainted-format/tainted-format-noncompliant.rb
+++ src/ruby/detectors/tainted-format/tainted-format-noncompliant.rb
@@ -3,7 +3,7 @@
  SPDX-License-Identifier: Apache-2.0
 =end
 
-# {fact rule=untrusted-format-strings@v1.0 defects=1}
+# {fact rule=tainted-format@v1.0 defects=1}
 class TaintedFormat < ActionController::Base
   def unsanitized_input
     # Noncompliant: untrusted user input is being used directly in format sting.
